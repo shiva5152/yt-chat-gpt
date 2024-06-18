fa462092bca6d418c9eba0afdfd4066df4fc70a6
shiva <ms454431@gmail.com>
2024-06-19 00:47:13 +0530

    webhook testing 2

--- src/app/lib/user.ts
+++ src/app/lib/user.ts
@@ -4,6 +4,7 @@ import connectToDb from "@/utils/connectDb";
 
 export const createUser = async (data: any) => {
     try {
+        console.log(data, "into user");
         await connectToDb();
         const user = await User.create(data);
         return { user }
