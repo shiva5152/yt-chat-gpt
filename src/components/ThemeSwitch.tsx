22df849668c743e18645eb250a9ef3f404b95249
shiva-metageeks <shiva@metageeks.tech>
2024-06-28 23:51:50 +0530

    dark theme 4.0

--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -4,6 +4,7 @@ import { FiSun, FiMoon } from "react-icons/fi";
 import { useState, useEffect } from "react";
 import { useTheme } from "next-themes";
 import Image from "next/image";
+// 
 
 const ThemeSwitch = () => {
   const [mounted, setMounted] = useState(false);
